54dbf6d036a10dc154f79e7d05d5fef0bc463731
cn_techlite <cntechlite@gmail.com>
2026-07-04 11:11:02 +0100

    Initial booking endpoint tester

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api-proxy": {
+        target: "https://api-data-connection.ginilog.org",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api-proxy/, ""),
+      },
+    },
+  },
+});
